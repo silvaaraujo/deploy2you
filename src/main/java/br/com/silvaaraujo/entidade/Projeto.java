85bfb00badb3bc53a19cf3b97c44443a75ac1787
Thiago Silva <thiagoocara@gmail.com>
2015-10-18 19:44:30 -0200

    ajuste na classe projeto

--- src/main/java/br/com/silvaaraujo/entidade/Projeto.java
+++ src/main/java/br/com/silvaaraujo/entidade/Projeto.java
@@ -1,9 +1,41 @@
 package br.com.silvaaraujo.entidade;
 
-public class Projeto {
+import java.io.Serializable;
 
+public class Projeto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String nome;
 	private String gitRepository;
+
+	public Projeto() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGitRepository() {
+		return gitRepository;
+	}
+
+	public void setGitRepository(String gitRepository) {
+		this.gitRepository = gitRepository;
+	}
 	
 }
