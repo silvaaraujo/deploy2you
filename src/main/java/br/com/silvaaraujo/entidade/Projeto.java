2da2f5caa2c627e58e4f84018b96dfd0bdf05af2
Thiago Silva <thiagoocara@gmail.com>
2015-10-18 21:04:45 -0200

    ajustes nas entidades

--- src/main/java/br/com/silvaaraujo/entidade/Projeto.java
+++ src/main/java/br/com/silvaaraujo/entidade/Projeto.java
@@ -8,7 +8,8 @@ public class Projeto implements Serializable {
 	
 	private Long id;
 	private String nome;
-	private String gitRepository;
+	private String repositorioGit;
+	private String nomeImagemDocker;
 
 	public Projeto() {
 		super();
@@ -30,12 +31,45 @@ public class Projeto implements Serializable {
 		this.nome = nome;
 	}
 
-	public String getGitRepository() {
-		return gitRepository;
+	public String getRepositorioGit() {
+		return repositorioGit;
 	}
 
-	public void setGitRepository(String gitRepository) {
-		this.gitRepository = gitRepository;
+	public void setRepositorioGit(String repositorioGit) {
+		this.repositorioGit = repositorioGit;
 	}
-	
+
+	public String getNomeImagemDocker() {
+		return nomeImagemDocker;
+	}
+
+	public void setNomeImagemDocker(String nomeImagemDocker) {
+		this.nomeImagemDocker = nomeImagemDocker;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Projeto other = (Projeto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
