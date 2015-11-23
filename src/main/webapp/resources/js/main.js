182f6da76e64248d0e8dfe896500138b2b7dff7e
Thiago Silva <thiagoocara@gmail.com>
2015-11-23 20:48:41 -0200

    adicionado componentes padrões para msgs: info, sucesso, erro e aviso

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -5,6 +5,29 @@
 	alerta.show = function(msg) { 
 		$.bootstrapGrowl(msg);
 	};
+	
+	alerta.info = function(msg) { 
+		$.bootstrapGrowl(msg, {
+			type: 'info'
+		});
+	};
+	
+	alerta.sucesso = function(msg) { 
+		$.bootstrapGrowl(msg, {
+			type: 'success'
+		});
+	};
+	
+	alerta.erro = function(msg) { 
+		$.bootstrapGrowl(msg, {
+			type: 'danger'
+		});
+	};
+	
+	alerta.aviso = function(msg) { 
+		$.bootstrapGrowl(msg, {
+			type: 'warning'
+		});
+	};
 
 })(jQuery);
-
